90a7c93a7bdd6724ff414a2188785ce99ea25402
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-15 12:17:17 +0530

    jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
 import insta from './assets/instagram.png'
-
 import './App.css'
 
 // logo component 
 function Logo(props){
-  return <img src={props.src} width={props.width} height={props.height} alt="LOGO" />
+  return <img src={props.src} width={props.width} height={props.height} alt="LOGO" className='logo' />
 }
 
 // signin component
 function Signin(){
 
   return(
-    <>
+    <div className='signin'>
       <input type="text" placeholder='Phone number, username, or email'/> <br/>
-      <input type="password" placeholder='password'/> <br/>
+      <input type="password" placeholder='Password'/> <br/>
       <button>Sign in </button>
-    </>
+    </div>
   )
 }
 
@@ -24,13 +23,13 @@ function Signin(){
 function Signup(){
 
   return(
-    <>
-      <input type="text" placeholder='Mobile number or Email'/> <br/>
+    <div className='signup'>
+      <input type="text" placeholder='Mobile Number or Email'/> <br/>
       <input type="text" placeholder='Full Name'/> <br/>
       <input type="text" placeholder='Phone number, username, or email'/> <br/>
-      <input type="text" placeholder='password'/> <br/>
+      <input type="password" placeholder='Password'/> <br/>
       <button>Sign up </button>
-    </>
+    </div>
   )
 }
 
@@ -38,33 +37,18 @@ function Signup(){
 function App() {
 
   const [login, setLogin] = useState(true)
-  // const [status, setStatus] = useState(login)
 
   return (
-    <>
-    <Logo src={insta} width="200px" height="100pxs"/>
-
+    <div className='conatiner'>
+    <Logo src={insta} width="200px" height="70pxs"/>
     <br/>
 
     {
       (login)? <Signin/> : <Signup/>
     }
-    {
-      // (login)? <input type="text" placeholder='Phone number, username or ,email'/> : <input type="text" placeholder='Mobile number or email full name'/>
-    }
-    {/* {
-      (!login)? <input type="text" placeholder='password'/> : <input type="text" placeholder='Phone number, username or ,email'/> && <input type="text" placeholder='password'/>
-    } */}
-
-
-    {/* <input type="text" placeholder={login} is { (login == {status}) ? 'Phone number, username or ,email': 'Mobile number or email full name'} />  */}
-
-    {/* <input type="text" /> */}
-    {/* <button> {(login)?'Sign in': 'Sign Up'} </button> */}
-    {(login)? <p>Don't have account ? <span onClick={()=>setLogin(!login)}> {(!login)?'Sign in': 'Sign up'}</span></p> : <p>Have an account ? <span onClick={()=>setLogin(!login)}> {(!login)?'Log in': 'Sign up'}</span></p>}
-    {/* <p/> */}
-    {/* <p></p> */}
-    </>
+    
+    {(login)? <p>Don't have account? <span onClick={()=>setLogin(!login)}> {(!login)?'Sign in': 'Sign up'}</span></p> : <p>have an account? <span onClick={()=>setLogin(!login)}> {(!login)?'Log in': 'Sign up'}</span></p>}
+    </div>
   )
 }
 
